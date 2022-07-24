6a77b717672b679fff94708f8260c045b45b1115
Andrew Zaiko <andrewzaiko@gmail.com>
2022-07-24 17:12:53 +0300

    Создал функцию load_posts

--- functions.py
+++ functions.py
@@ -1,1 +1,20 @@
+# Импортируем модуль JSON для работы с этим форматом
 import json
+from json import JSONDecodeError
+
+
+def load_posts(file_with_posts):
+    """
+    :param file_with_posts: Файл с постами формата JSON
+    :return: Список постов
+    """
+    try:
+        file = open(file_with_posts, encoding="utf-8")
+        posts = json.loads(file.read())
+    except FileNotFoundError:
+        quit("Файл с постами не найден")
+    except JSONDecodeError:
+        quit("Файл с постами не удалось считать")
+    else:
+        file.close()
+        return posts
