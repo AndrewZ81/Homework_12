5e68022992d28451692d85631cadeef82540e8f8
Andrew Zaiko <andrewzaiko@gmail.com>
2022-07-24 19:59:21 +0300

    Связал форму поиска с маршрутом

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 # Импортируем необходимые инструменты из модуля flask
-from flask import Flask, render_template, send_from_directory
+from flask import Flask, render_template, send_from_directory, request
 
 # Импортируем модуль functions для работы с форматом JSON
 import functions
 
 app = Flask(__name__)
 
+
+# Создаём корневой маршрут
 @app.route("/")
 def main_page():
     """
@@ -14,9 +16,18 @@ def main_page():
     return render_template("index.html")
 
 
-@app.route("/list")
-def page_tag():
-    pass
+# Создаём маршрут для поиска постов по ключу
+@app.route("/search")
+def page_with_selected_posts():
+    """
+    :return: Заполненный шаблон всех постов с данным ключом
+    """
+    key_for_search = request.args.get("s")
+    list_of_posts_for_output = []
+    for i in functions.load_posts("posts.json"):
+        if key_for_search.lower() in i["content"].lower():
+            list_of_posts_for_output.append(i)
+    return render_template("posts_list.html", _list=list_of_posts_for_output, key=key_for_search)
 
 
 @app.route("/post", methods=["GET", "POST"])
