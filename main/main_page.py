1bbc2fb76158bd28c56d0227ecf682befab93023
Andrew Zaiko <andrewzaiko@gmail.com>
2022-07-24 17:12:16 +0300

    Переименовал файл и блупринт

---
+++ main/main_page.py
@@ -0,0 +1,8 @@
+from flask import Blueprint, render_template
+
+main_page_blueprint = Blueprint("main_page_blueprint", __name__, template_folder="../templates")
+
+
+@main_page_blueprint.route("/")
+def main_page():
+    return render_template("index.html")
