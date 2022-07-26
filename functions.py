b46727f0df210d0710a834f4c1fee0c804d3bede
Andrew Zaiko <andrewzaiko@gmail.com>
2022-07-26 15:03:07 +0300

    Добавил функцию проверки расширения загружаемого файла и обновил эндпоинт страницы созданного поста

--- functions.py
+++ functions.py
@@ -18,3 +18,16 @@ def load_posts(file_with_posts):
     else:
         file.close()
         return posts
+
+
+def is_extension_allowed(file_name):
+    """
+    :param file_name: Полное имя загружаемого файла при создании поста
+    :return: Логическое значение по результату проверки допустимости расширения файла
+    """
+    allowed_extensions = {"jpg", "jpeg", "bmp", "png"}
+    extension = file_name.split(".")[-1]
+    if extension in allowed_extensions:
+        return True
+    else:
+        return False
--- post/views.py
+++ post/views.py
@@ -1,8 +1,8 @@
 # Подключаем для создания блюпринтов на основе шаблонов
 from flask import Blueprint, render_template, request
 
-# Подключаем инструменты модуля functions для работы с форматом JSON
-from functions import load_posts
+# Подключаем инструменты модуля functions
+from functions import is_extension_allowed
 
 # Создаём блюпринты страниц создания нового поста и созданного поста, соответственно
 post_form_blueprint = Blueprint("post_form_blueprint", __name__, template_folder="templates")
@@ -23,9 +23,16 @@ def page_post_form():
 def page_post_upload():
     picture = request.files.get("picture")
     if picture:
-        picture.save(f"./uploads/images/{picture.filename}")
-        user_post = request.form.get("content")
-        return render_template("post_uploaded.html", user_post=user_post, user_picture=picture.filename)
+        file_name = picture.filename
+        if is_extension_allowed(file_name):
+            picture.save(f"./uploads/images/{picture.filename}")
+            user_post = request.form.get("content")
+            return render_template("post_uploaded.html", user_post=user_post, user_picture=picture.filename)
+        else:
+            return f"<pre><link rel='stylesheet' href='/static/style.css'>" \
+                   f"<p>Расширение выбранного файла недопустимо." \
+                   f"Вернитесь назад и попробуйте снова</p>" \
+                   f"<a href='post' class='button'>Назад</a></pre>"
     else:
         return f"<pre><link rel='stylesheet' href='/static/style.css'>" \
                f"<p>Вы не загрузили файл. Вернитесь назад и попробуйте снова</p>" \
