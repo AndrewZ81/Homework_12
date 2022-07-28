c52efec0f488601f02e173c78131350787607df2
Andrew Zaiko <andrewzaiko@gmail.com>
2022-07-28 15:13:23 +0300

    Добавил логирование поиска постов и неуспешной загрузки файлов к постам

--- post/views.py
+++ post/views.py
@@ -4,9 +4,20 @@ from flask import Blueprint, render_template, request
 # Подключаем инструменты модуля functions
 from functions import is_extension_allowed, save_post
 
+# Подключаем инструменты логирования
+import logging
+
 # Создаём блюпринты страниц создания нового поста и созданного поста, соответственно
 post_form_blueprint = Blueprint("post_form_blueprint", __name__, template_folder="templates")
 post_uploaded_blueprint = Blueprint("post_uploaded_blueprint", __name__, template_folder="templates")
+
+# Настраиваем логирование
+extension_checker_logger = logging.getLogger(__name__)
+extension_checker_logger.setLevel(logging.INFO)
+extension_checker_handler = logging.FileHandler("ext_check_log.txt", encoding="utf-8")
+extension_checker_formatter = logging.Formatter("%(asctime)s : %(message)s")
+extension_checker_handler.setFormatter(extension_checker_formatter)
+extension_checker_logger.addHandler(extension_checker_handler)
 
 
 @post_form_blueprint.route("/post", methods=["GET"])
@@ -28,11 +39,13 @@ def page_post_upload():
     if picture:
         file_name = picture.filename
         if is_extension_allowed(file_name):
+            extension_checker_logger.info(f"Загружен файл {picture.filename}")
             picture.save(f"./uploads/images/{picture.filename}")
             user_post = request.form.get("content")
             save_post("posts.json", {'pic': f"./uploads/images/{picture.filename}", 'content': f"{user_post}"})
             return render_template("post_uploaded.html", user_post=user_post, user_picture=picture.filename)
         else:
+            extension_checker_logger.info(f"Расширение выбранного файла {picture.filename} недопустимо")
             return f"<pre><link rel='stylesheet' href='/static/style.css'>" \
                    f"<p>Расширение выбранного файла недопустимо. " \
                    f"Вернитесь назад и попробуйте снова</p>" \
--- search/views.py
+++ search/views.py
@@ -1,8 +1,22 @@
 # Подключаем для создания блюпринтов на основе шаблонов
+import logging
+
 from flask import Blueprint, render_template, request
+
+# Подключаем инструменты логирования
+import logging
 
 # Подключаем инструменты модуля functions для работы с форматом JSON
 from functions import load_posts
+
+# Настраиваем логирование
+searching_logger = logging.getLogger(__name__)  # для логирования поиска постов
+searching_logger.setLevel(logging.INFO)
+searching_handler = logging.FileHandler("search_log.txt", encoding="utf-8")
+searching_formatter = logging.Formatter("%(asctime)s : %(message)s")
+searching_handler.setFormatter(searching_formatter)
+searching_logger.addHandler(searching_handler)
+
 
 # Создаём блюпринт страницы поиска по постам
 search_blueprint = Blueprint("search_blueprint", __name__, template_folder="templates")
@@ -16,11 +30,13 @@ def page_with_selected_posts():
     """
     key_for_search = request.args.get("s")
     if not key_for_search:
+        searching_logger.info("Не введен текст для поиска")
         return f"<pre><link rel='stylesheet' href='/static/style.css'>" \
                 f"<p>Вы не ввели текст для поиска. " \
                 f"Вернитесь назад и попробуйте снова</p>" \
                 f"<a href='/' class='button'>Назад</a></pre>"
     else:
+        searching_logger.info(f"Выполнен поиск {key_for_search}")
         list_of_posts_for_output = []
         for i in load_posts("posts.json"):
             if key_for_search.lower() in i["content"].lower():
