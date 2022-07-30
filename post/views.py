f9f9be77c07d71b00346c747251638fe7b5a70a6
Andrew Zaiko <andrewzaiko@gmail.com>
2022-07-30 19:43:56 +0300

    Добавил логирование отсутствующего файла при создании поста

--- post/views.py
+++ post/views.py
@@ -12,12 +12,19 @@ post_form_blueprint = Blueprint("post_form_blueprint", __name__, template_folder
 post_uploaded_blueprint = Blueprint("post_uploaded_blueprint", __name__, template_folder="templates")
 
 # Настраиваем логирование
-extension_checker_logger = logging.getLogger(__name__)
+extension_checker_logger = logging.getLogger("extension_checker_logger")
 extension_checker_logger.setLevel(logging.INFO)
-extension_checker_handler = logging.FileHandler("ext_check_log.txt", encoding="utf-8")
+extension_checker_handler = logging.FileHandler("file_extension_log.txt", encoding="utf-8")
 extension_checker_formatter = logging.Formatter("%(asctime)s : %(message)s")
 extension_checker_handler.setFormatter(extension_checker_formatter)
 extension_checker_logger.addHandler(extension_checker_handler)
+
+loading_checker_logger = logging.getLogger("loading_checker_logger")
+loading_checker_logger.setLevel(logging.ERROR)
+loading_checker_handler = logging.FileHandler("file_load_log.txt", encoding="utf-8")
+loading_checker_formatter = logging.Formatter("%(asctime)s : %(message)s")
+loading_checker_handler.setFormatter(loading_checker_formatter)
+loading_checker_logger.addHandler(loading_checker_handler)
 
 
 @post_form_blueprint.route("/post", methods=["GET"])
@@ -39,18 +46,19 @@ def page_post_upload():
     if picture:
         file_name = picture.filename
         if is_extension_allowed(file_name):
-            extension_checker_logger.info(f"Загружен файл {picture.filename}")
+            extension_checker_logger.info(f"Загружен файл '{picture.filename}'")
             picture.save(f"./uploads/images/{picture.filename}")
             user_post = request.form.get("content")
             save_post("posts.json", {'pic': f"./uploads/images/{picture.filename}", 'content': f"{user_post}"})
             return render_template("post_uploaded.html", user_post=user_post, user_picture=picture.filename)
         else:
-            extension_checker_logger.info(f"Расширение выбранного файла {picture.filename} недопустимо")
+            extension_checker_logger.info(f"Расширение выбранного файла '{picture.filename}' недопустимо")
             return f"<pre><link rel='stylesheet' href='/static/style.css'>" \
                    f"<p>Расширение выбранного файла недопустимо. " \
                    f"Вернитесь назад и попробуйте снова</p>" \
                    f"<a href='/post' class='button'>Назад</a></pre>"
     else:
+        loading_checker_logger.error(f"Файл не загружен")
         return f"<pre><link rel='stylesheet' href='/static/style.css'>" \
                f"<p>Вы не загрузили файл. Вернитесь назад и попробуйте снова</p>" \
                f"<a href='/post' class='button'>Назад</a></pre>"
